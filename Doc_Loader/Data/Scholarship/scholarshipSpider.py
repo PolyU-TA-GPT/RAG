d1038672525ddcff0133dc8770da59e2aad73b34
DavidDING21 <136289301+DavidDING21@users.noreply.github.com>
2024-02-12 16:56:12 +0800

    Add files related to scholarship

---
+++ Doc_Loader/Data/Scholarship/scholarshipSpider.py
@@ -0,0 +1,48 @@
+import scrapy
+from scrapy.crawler import CrawlerProcess
+
+head = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+}
+
+class Scholarship_Web_Spider(scrapy.Spider):
+    name = "scholarship_web_spider"
+
+    def start_requests(self):
+        yield scrapy.Request(url="https://www.polyu.edu.hk/sao/student-resources-and-support-section/scholarships/scholarship-types/",
+                             callback=self.parse, headers=head)
+
+    def parse(self, response):
+        # Get all the pure text inside <main class="page-content">
+        text = response.css("main.page-content ::text").getall()
+        pure_text = ' '.join(text).strip()
+        pure_text = pure_text.replace('\n', ' ').replace('  ', ' ')
+        filepath = 'scholarship_info.txt'
+        with open(filepath, 'w') as f:
+            f.write(pure_text)
+
+class Scholarship_Link_Spider(scrapy.Spider):
+    name = "scholarship_link_spider"
+
+    def start_requests(self):
+        yield scrapy.Request(url="https://www.polyu.edu.hk/sao/student-resources-and-support-section/scholarships/scholarship-types/",
+                             callback=self.parse, headers=head)
+
+    def parse(self, response):
+        # Get all the picture links inside <main class="page-content">
+        picture_links = response.css("main.page-content a::attr(href)").getall()
+        filepath = 'scholarship_links.txt'
+        with open(filepath, 'w') as f:
+            f.write("Here are some useful links for scholarships:\n")
+            for link in picture_links:
+                # Delete if the link begins with "#!"
+                if link.startswith("#!"):
+                    continue
+                if link.startswith("/"):
+                    link = "https://www.polyu.edu.hk" + link
+                f.write(link + "\n")
+
+process = CrawlerProcess()
+# process.crawl(Scholarship_Web_Spider)
+process.crawl(Scholarship_Link_Spider)
+process.start()
